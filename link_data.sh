d0dc1e6b11115906157d61bdcdfabe1ec55a1343
YanyiPU <53686732+YanyiPU@users.noreply.github.com>
2023-03-13 23:24:30 +0000

    Create link_data.sh

---
+++ link_data.sh
@@ -0,0 +1,12 @@
+granularity="second"
+method="EdgeBank"
+
+
+for data in wikipedia reddit mooc lastfm enron SocialEvo uci; do
+    # link the npy files
+    ln -s /home/silence/Desktop/OneDrive/L45-project/DG_data/TG_network_datasets/"$data"/ml_"$data"_node.npy $method/data/ml_"$data"_node.npy 
+    ln -s /home/silence/Desktop/OneDrive/L45-project/DG_data/TG_network_datasets/"$data"/ml_"$data".npy $method/data/ml_"$data".npy
+
+    # link the csv files
+    ln -s /home/silence/Desktop/OneDrive/L45-project/DG_data_coarse/$data/$granularity/ml_$data.csv $method/data/ml_$data.csv
+done
